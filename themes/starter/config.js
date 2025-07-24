60e768d72fb8a2dce55c57e3aa4c0591b316eea1
balanceboardtop <18279166486@163.com>
2025-07-24 22:44:30 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -15,13 +15,13 @@ const CONFIG = {
   // 英雄区两个按钮，如果TEXT留空则隐藏按钮
   STARTER_HERO_BUTTON_1_TEXT: 'Get started', // 英雄区按钮
   STARTER_HERO_BUTTON_1_URL:
-    'https://docs.tangly1024.com/article/vercel-deploy-notion-next', // 英雄区按钮
-  STARTER_HERO_BUTTON_2_TEXT: '在Github上关注', // 英雄区按钮
-  STARTER_HERO_BUTTON_2_URL: 'https://www.amazon.com/dp/B0D29K5WDC?maas=maas_adg_260472CD1A7F5389C6D46E346628CD3F_afap_abs&ref_=aa_maas&tag=maas', // 英雄区按钮
+    'https://www.amazon.com/dp/B0D29K5WDC?maas=maas_adg_260472CD1A7F5389C6D46E346628CD3F_afap_abs&ref_=aa_maas&tag=maas', // 英雄区按钮
+  STARTER_HERO_BUTTON_2_TEXT: '', // 英雄区按钮
+  STARTER_HERO_BUTTON_2_URL: '', // 英雄区按钮
   STARTER_HERO_BUTTON_2_ICON: '', // 英雄区按钮2的图标，不需要则留空
 
   // 英雄区配图，如需隐藏，改为空值即可 ''
-  STARTER_HERO_PREVIEW_IMAGE: '/images/starter/hero/hero-image.webp', // 产品预览图 ，默认读取public目录下图片
+  STARTER_HERO_PREVIEW_IMAGE: '', // 产品预览图 ，默认读取public目录下图片
   STARTER_HERO_BANNER_IMAGE: '', // hero区下方的全宽图
 
   // 顶部右侧导航暗流
