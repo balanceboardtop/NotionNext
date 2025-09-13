06f10d262e2624979f9b9857c1924e3cb313c1d9
balanceboardtop <18279166486@163.com>
2025-09-13 16:56:00 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -15,14 +15,14 @@ const CONFIG = {
   // 英雄区两个按钮，如果TEXT留空则隐藏按钮
   STARTER_HERO_BUTTON_1_TEXT: 'Get started', // 英雄区按钮
   STARTER_HERO_BUTTON_1_URL:
-    'https://www.amazon.com/dp/B0D29K5WDC?maas=maas_adg_260472CD1A7F5389C6D46E346628CD3F_afap_abs&ref_=aa_maas&tag=maas', // 英雄区按钮
+    'https://www.amazon.com/dp/B0D9SLV33R', // 英雄区按钮
   STARTER_HERO_BUTTON_2_TEXT: 'Get installation instructions', // 英雄区按钮
   STARTER_HERO_BUTTON_2_URL: 'https://drive.google.com/drive/folders/15a_QDrlSw7c44bPgJr_na67UDcNdX1zJ?usp=drive_link', // 英雄区按钮
   STARTER_HERO_BUTTON_2_ICON: '', // 英雄区按钮2的图标，不需要则留空
 
   // 英雄区配图，如需隐藏，改为空值即可 ''
   STARTER_HERO_PREVIEW_IMAGE: '/images/starter/hero/主图设计.png', // 产品预览图 ，默认读取public目录下图片
-  STARTER_HERO_BANNER_IMAGE: '/images/starter/hero/1.png', // hero区下方的全宽图
+  STARTER_HERO_BANNER_IMAGE: '', // hero区下方的全宽图
 
   // 顶部右侧导航暗流
   STARTER_NAV_BUTTON_1_TEXT: 'Sign In',
@@ -60,17 +60,17 @@ const CONFIG = {
   STARTER_FEATURE_4_BUTTON_URL: 'balanceboard.top', // 特性4
 
   // 首页ABOUT区块
-  STARTER_ABOUT_ENABLE: false, // ABOUT区块开关
-  STARTER_ABOUT_TITLE: '一套轻量实用的建站解决方案',
+  STARTER_ABOUT_ENABLE: true, // ABOUT区块开关
+  STARTER_ABOUT_TITLE: 'A balance trainer that works your core muscles, improves posture, and increases joint flexibility',
   STARTER_ABOUT_TEXT:
-    'NotionNext的愿景是帮助非技术人员的小白，最低成本、最快速地搭建自己的网站，帮助您将自己的产品与故事高效地传达给世界。 <br /> <br /> 功能强大的Notion笔记，简单快速的Vercel托管平台，组成一个简单的网站',
-  STARTER_ABOUT_BUTTON_TEXT: '了解更多',
+    'Toibena vision is to help you find your balance and experience the beauty of life through a stronger sense of balance, so you can perform at your best and face the world safely!',
+  STARTER_ABOUT_BUTTON_TEXT: 'Learn more',
   STARTER_ABOUT_BUTTON_URL: 'https://docs.tangly1024.com/about',
   STARTER_ABOUT_IMAGE_1: '/images/starter/about/about-image-01.jpg',
   STARTER_ABOUT_IMAGE_2: '/images/starter/about/about-image-02.jpg',
-  STARTER_ABOUT_TIPS_1: '7000+',
-  STARTER_ABOUT_TIPS_2: '博客站点',
-  STARTER_ABOUT_TIPS_3: '正在线上运行',
+  STARTER_ABOUT_TIPS_1: '3000+',
+  STARTER_ABOUT_TIPS_2: 'Sports Expert',
+  STARTER_ABOUT_TIPS_3: 'Practicing balance',
 
   // 首页价格区块
   STARTER_PRICING_ENABLE: false, // 价格区块开关
